b9b1643aefbba128b3bd31ed6635b4ca9317ab96
Tharathorn Inphen <t.earendel@gmail.com>
2013-08-16 18:31:27 +0700

    Mocked JSON response for suggest page

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,6 +6,11 @@ angular.module('myApp.controllers', []).
   controller('HomeController', [function() {
 
   }])
-  .controller('SuggestController', [function() {
-
+  .controller('SuggestController', ['$scope', function($scope) {
+    $scope.dish = {
+      restaurantName: 'Madam-T',
+      price: 400000,
+      imagePath: '/app/images/food1.jpg',
+      name: ''
+    };
   }]);
