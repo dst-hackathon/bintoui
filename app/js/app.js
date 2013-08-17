427e996d1457059ee13618a0b2af839aee9b1f63
mekeerati <me.keerati@gmail.com>
2013-08-17 13:38:04 +0700

    add "add page". Signed-off-by:mekeerati <me.keerati@gmail.com>

--- app/js/app.js
+++ app/js/app.js
@@ -6,5 +6,6 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'HomeController'});
     $routeProvider.when('/suggest', {templateUrl: 'partials/suggest.html', controller: 'SuggestController'});
+    $routeProvider.when('/add', {templateUrl: 'partials/add.html', controller: 'AddController'});
     $routeProvider.otherwise({redirectTo: '/home'});
   }]);
